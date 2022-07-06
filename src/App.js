e6585195ba4f8ccb1ff0f54c4341df5b753af91a
Afrizal <afrizalbagas.03@gmail.com>
2022-07-06 11:38:23 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
 
-import { ThemeProvider } from '@mui/material';
+import { ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 
-import Navbar from './components/Navbar';
-import theme from './themes/theme';
+import Navbar from "./components/Navbar";
+import MovieList from "./containers/MovieList";
+import theme from "./themes/theme";
+import { Box } from "@mui/system";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Navbar></Navbar>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route
+            path="indonesian"
+            element={<Box sx={{ mt: 10 }}>Halaman Indonesian</Box>}
+          />
+          <Route
+            path="pricing"
+            element={<Box sx={{ mt: 10 }}>Halaman Pricing</Box>}
+          />
+          <Route
+            path="about"
+            element={<Box sx={{ mt: 10 }}>Halaman About</Box>}
+          />
+        </Routes>
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
